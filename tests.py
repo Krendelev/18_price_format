574ad591b825e8fe4c5d2f04304d6d959866a483
Krendelev <krendelev@gmail.com>
2018-09-26 00:09:13 +0300

    add more tests

--- tests.py
+++ tests.py
@@ -4,23 +4,38 @@ from format_price import format_price
 
 class TestPriceFormat(unittest.TestCase):
 
-    def test_number(self):
+    def test_number_as_digits(self):
+        self.assertEqual(format_price(3245.000000), '3 245')
+
+    def test_number_as_string(self):
         self.assertEqual(format_price('3245.000000'), '3 245')
 
+    def test_number_not_whole(self):
+        self.assertEqual(format_price('3245.67'), '3 245.67')
+
+    def test_number_is_negative(self):
+        self.assertEqual(format_price('-3245.67'), '-3 245.67')
+
+    def test_zero(self):
+        self.assertEqual(format_price(0), '0')
+
+    def test_big_number(self):
+        self.assertEqual(format_price('1234567890'), '1 234 567 890')
+
     def test_text(self):
-        self.assertRaises(ValueError, format_price, 'test')
-
-    def test_list(self):
-        self.assertRaises(TypeError, format_price, [3245])
-
-    def test_dict(self):
-        self.assertRaises(TypeError, format_price, {'price': 3245})
+        self.assertIsNone(format_price('test'))
 
     def test_bool(self):
-        self.assertRaises(TypeError, format_price, False)
+        self.assertIsNone(format_price(True))
 
     def test_None(self):
-        self.assertRaises(TypeError, format_price, None)
+        self.assertIsNone(format_price(None))
+
+    def test_list(self):
+        self.assertIsNone(format_price([3245]))
+
+    def test_dict(self):
+        self.assertIsNone(format_price({'price': 3245}))
 
 
 if __name__ == '__main__':
